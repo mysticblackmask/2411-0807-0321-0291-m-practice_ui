94086b424897198e7855afac1ba0be85cd45ddd8
mysticblackmask <black.mask0209@outlook.com>
2024-11-05 05:15:58 -0800

    divide route and dash

---
+++ src/components/styles/RoutesIndex.ts
@@ -0,0 +1,26 @@
+import { styled } from "@mui/material/styles";
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
+  open?: boolean;
+}>(({ theme }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        transition: theme.transitions.create("margin", {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+      },
+    },
+  ],
+}));
+
+export { Main };
