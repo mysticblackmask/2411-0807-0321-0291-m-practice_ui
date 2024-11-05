8754405c78cf7e4ab003d32d7da9fec58be1919c
mysticblackmask <black.mask0209@outlook.com>
2024-11-05 03:55:36 -0800

    custom appHook

---
+++ src/components/hooks/AppHooks.tsx
@@ -0,0 +1,41 @@
+import React, { FC, MouseEvent } from "react";
+import { Box, Fade, useScrollTrigger } from "@mui/material";
+import { ScrollProps } from "../../types/App";
+
+const ScrollTopHook: FC<ScrollProps> = (props) => {
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const { window, children } = props;
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event: MouseEvent<HTMLDivElement>) => {
+    const anchor = (
+      (event.target as HTMLDivElement).ownerDocument || document
+    ).querySelector("#back-to-top-anchor");
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: "center",
+      });
+    }
+  };
+
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: "fixed", bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Fade>
+  );
+};
+
+export default ScrollTopHook;
