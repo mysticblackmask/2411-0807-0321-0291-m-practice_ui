6d6ccc4d3073de4e0fc6afd08bc27f8d213fe7f9
mysticblackmask <black.mask0209@outlook.com>
2024-11-05 03:56:38 -0800

    format functions

---
+++ src/components/styles/Index.ts
@@ -0,0 +1,17 @@
+import {
+  MtagSearch,
+  SearchIconWrapper,
+  StyledInputBase,
+  MtagAppBar,
+} from "./Header";
+import { DrawerHeader, openedMixin, closedMixin } from "./Sider";
+
+export {
+  MtagSearch,
+  SearchIconWrapper,
+  StyledInputBase,
+  MtagAppBar,
+  DrawerHeader,
+  openedMixin,
+  closedMixin,
+};
