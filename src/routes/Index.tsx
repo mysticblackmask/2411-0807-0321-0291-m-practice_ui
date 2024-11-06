5eee5cfe75e93ffeb650eaafddd85c2b66e8e989
mysticblackmask <black.mask0209@outlook.com>
2024-11-06 04:43:59 -0800

    change route and pages

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,19 +1,34 @@
-import React, { FC, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { FC, useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
 import { Toolbar, CssBaseline, Box } from "@mui/material";
 import { Main } from "../components/styles/Index";
+import { useAppSelector } from "../redux/store";
 
 import LayoutHeader from "../components/layouts/Header";
 import LayoutSider from "../components/layouts/Sider";
 import DashIndex from "../components/pages/dash/Index";
+import SignIn from "../components/pages/auth/SignIn";
+import SettingIndex from "../components/pages/settings/Index";
 
 const RoutesIndex: FC = () => {
   const [open, setOpen] = useState(0);
-
+  const isLogined = useAppSelector((state) => state.auth.isAuthenticated);
   const handleDrawer = (value: number) => {
     setOpen((pre) => value);
   };
+  useEffect(() => {
+    handleDrawer(0);
+  }, [isLogined]);
 
+  const ProtectedRoute = () => {
+    return isLogined ? <Outlet /> : <Navigate to="/signin" />;
+  };
   return (
     <Router>
       <Box sx={{ display: "flex" }}>
@@ -23,7 +38,12 @@ const RoutesIndex: FC = () => {
         <Main open={open > 0 ? true : false}>
           <Toolbar id="back-to-top-anchor" />
           <Routes>
+            <Route element={ProtectedRoute()}>
+              <Route path="/setting" element={<SettingIndex />} />
+            </Route>
             <Route path="/" element={<DashIndex />} />
+            <Route path="/singin" element={<SignIn />} />
+            <Route path="*" element={<SignIn />} />
           </Routes>
         </Main>
       </Box>
