b3b7f7cd3353366b41ff04da54c789a6968ebe30
mysticblackmask <black.mask0209@outlook.com>
2024-11-07 09:30:48 -0800

    add color system

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import { ColorProvider } from "./components/contexts/Index";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,7 +14,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ColorProvider>
+        <App />
+      </ColorProvider>
     </Provider>
   </React.StrictMode>
 );
