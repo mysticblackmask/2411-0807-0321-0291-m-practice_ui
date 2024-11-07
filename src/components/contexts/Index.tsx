5938451671d5b043b551bb4fd463b3894088b71f
mysticblackmask <black.mask0209@outlook.com>
2024-11-07 09:33:59 -0800

    create context

---
+++ src/components/contexts/Index.tsx
@@ -0,0 +1,40 @@
+import { useState, createContext, ReactNode } from "react";
+import clr from "./themes/Index";
+import { ColorContextType } from "./contexts";
+import { themeType } from "./themes/theme";
+
+export const ColorContext = createContext<ColorContextType | undefined>(
+  undefined
+);
+
+export const ColorProvider = ({ children }: { children: ReactNode }) => {
+  const [color, setColor] = useState<themeType>(clr.primary);
+  const handleColor = (value?: string) => {
+    switch (value) {
+      case "primary":
+        setColor(clr.primary);
+        break;
+      case "error":
+        setColor(clr.error);
+        break;
+      case "secondary":
+        setColor(clr.secondary);
+        break;
+      case "info":
+        setColor(clr.info);
+        break;
+      case "success":
+        setColor(clr.success);
+        break;
+      default:
+        setColor(clr.warning);
+        break;
+    }
+  };
+
+  return (
+    <ColorContext.Provider value={{ color, handleColor }}>
+      {children}
+    </ColorContext.Provider>
+  );
+};
---
+++ src/components/contexts/themes/theme.d.ts
@@ -0,0 +1,4 @@
+export interface themeType {
+  back: string;
+  badge: string;
+}
