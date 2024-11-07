680902fb052e232b432c64109de1fb79686eb992
mysticblackmask <black.mask0209@outlook.com>
2024-11-07 09:37:22 -0800

    collection hooks and components data

---
+++ src/components/constants/Index.ts
@@ -0,0 +1,5 @@
+import { SiderData } from "./SiderData";
+import { HeaderHooksData } from "./HeaderHooksData";
+import { RouteIndexData } from "./RouteIndexData";
+
+export { SiderData, HeaderHooksData, RouteIndexData };
