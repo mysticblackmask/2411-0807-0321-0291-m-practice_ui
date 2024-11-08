5866f7884d578b5d708ca267d1bdf14f21cbb758
mysticblackmask <black.mask0209@outlook.com>
2024-11-08 10:42:48 -0800

    divide dash

--- src/components/pages/dash/Index.tsx
+++ src/components/pages/dash/Index.tsx
@@ -1,94 +1,195 @@
-import React, { useContext, useState } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  ReactNode,
+  SetStateAction,
+} from "react";
 import {
   Grid2,
-  ImageList,
-  ImageListItem,
-  ImageListItemBar,
-  IconButton,
-  CardActionArea,
+  styled,
+  Paper,
+  Box,
+  LinearProgress,
+  Typography,
+  useTheme,
 } from "@mui/material";
-import { LineChart } from "@mui/x-charts";
-import { Info } from "@mui/icons-material";
+import { PieChart, useDrawingArea } from "@mui/x-charts";
+import DashImages from "./Images";
+import { DashChart } from "./Chart";
+import DashTable from "./Table";
+import { ArrayNumbers, randomNumber } from "../../../utils/Funcs";
+import { leng } from "../../constants/DashImage";
 
-function srcset(image: string, size: number, rows = 1, cols = 1) {
-  return {
-    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-    srcSet: `${image}?w=${size * cols}&h=${
-      size * rows
-    }&fit=crop&auto=format&dpr=2 2x`,
-  };
+export interface chartDataType {
+  label: string;
+  data: number[];
 }
 
+export const desktopOS = [
+  {
+    label: "Windows",
+    value: 72.72,
+  },
+  {
+    label: "OS X",
+    value: 16.38,
+  },
+  {
+    label: "Linux",
+    value: 3.83,
+  },
+  {
+    label: "Chrome OS",
+    value: 2.42,
+  },
+  {
+    label: "Other",
+    value: 4.65,
+  },
+];
+
+const StyledText = styled("text")(({ theme }) => ({
+  fill: theme.palette.text.primary,
+  textAnchor: "middle",
+  dominantBaseline: "central",
+  fontSize: 20,
+}));
+
+function PieCenterLabel({ children }: { children: React.ReactNode }) {
+  const { width, height, left, top } = useDrawingArea();
+  return (
+    <StyledText x={left + width / 2} y={top + height / 2}>
+      {children}
+    </StyledText>
+  );
+}
 const DashIndex = () => {
-  const [data, setData] = useState();
+  const theme = useTheme();
+
+  const chartColors = [
+    theme.palette.primary.light,
+    theme.palette.error.light,
+    theme.palette.secondary.light,
+    theme.palette.warning.light,
+    theme.palette.success.light,
+    theme.palette.info.light,
+  ];
+  const [progress, setProgress] = useState<number>(0);
+  const [buffer, setBuffer] = useState<number>(1);
+  const timerRef = useRef<number>(0);
+  const progressRef = useRef(() => {});
+  useEffect(() => {
+    timerRef.current = window.setInterval(() => {
+      progressRef.current();
+    }, 150);
+    return () => {
+      clearInterval(timerRef.current);
+    };
+  }, []);
+  useEffect(() => {
+    progressRef.current = () => {
+      if (progress < 100) {
+        setProgress(progress + 1);
+        const newBuffer = progress + randomNumber(5, 1);
+        setBuffer(newBuffer > 100 ? 100 : newBuffer);
+      } else {
+        clearInterval(timerRef.current);
+      }
+    };
+  });
+
+  const imageItems = imageData.map((e: any) => ({
+    ...e,
+    data: ArrayNumbers(250, leng, 120),
+  }));
+  const valueFormatter = (item: { value: number }) => `${item.value}%`;
+  const [data, setData] = useState<chartDataType[]>([
+    {
+      data: imageItems[0].data,
+      label: imageItems[0].title,
+    },
+    {
+      data: ArrayNumbers(200, leng),
+      label: "Others",
+    },
+  ]);
+
+  const handleData = (value: chartDataType[]) => {
+    setData(value);
+  };
   return (
     <>
       <h1>DashBoard</h1>
-      <Grid2 container>
-        <Grid2 size={{ xs: 12, md: 6 }}>
-          <ImageList
-            sx={{ height: 450 }}
-            variant="quilted"
-            cols={4}
-            rowHeight={121}
-          >
-            {itemData.map((item) => (
-              <ImageListItem
-                key={item.img}
-                cols={item.cols || 1}
-                rows={item.rows || 1}
-              >
-                <img
-                  {...srcset(item.img, 121, item.rows, item.cols)}
-                  alt={item.title}
-                  loading="lazy"
+      <Grid2
+        container
+        rowSpacing={{ xs: 5, md: 10 }}
+        spacing={{ xs: 2, md: 4 }}
+      >
+        <DashImages imageData={imageItems} handleData={handleData}></DashImages>
+        <DashChart data={data}></DashChart>
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <Paper variant="outlined" square={false} sx={{ padding: 3 }}>
+            <PieChart
+              height={450}
+              tooltip={{ trigger: "item" }}
+              colors={chartColors}
+              slotProps={{
+                legend: { hidden: true },
+              }}
+              sx={{ left: 0, right: 0, width: "100%" }}
+              series={[
+                {
+                  innerRadius: 90,
+                  outerRadius: 160,
+                  data: desktopOS,
+                  arcLabel: (params) => params.label ?? "",
+                  arcLabelMinAngle: 20,
+                  highlightScope: { fade: "global", highlight: "item" },
+                  faded: {
+                    innerRadius: 9,
+                    additionalRadius: -22.5,
+                    color: "gray",
+                  },
+                  valueFormatter,
+                },
+              ]}
+            >
+              <PieCenterLabel>ASD</PieCenterLabel>
+            </PieChart>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Box sx={{ width: "100%", mr: 1 }}>
+                <LinearProgress
+                  sx={{ height: 10, borderRadius: 5 }}
+                  variant="buffer"
+                  value={progress}
+                  valueBuffer={buffer}
                 />
-                <ImageListItemBar
-                  title={item.title}
-                  subtitle={item.author}
-                  actionIcon={
-                    <IconButton
-                      sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                      aria-label={`info about ${item.title}`}
-                    >
-                      <Info />
-                    </IconButton>
-                  }
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
+              </Box>
+              <Box>
+                <Typography
+                  variant="body2"
+                  sx={{ color: "text.secondary" }}
+                >{`${Math.round(progress)}%`}</Typography>
+              </Box>
+            </Box>
+          </Paper>
         </Grid2>
-        <Grid2 size={{ xs: 12, md: 6 }}>
-          <LineChart
-            xAxis={[
-              {
-                data: [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024],
-                scaleType: "point",
-              },
-            ]}
-            series={[
-              {
-                data: [123, 432, 543, 6, 345, 435, 43, 234],
-                label: "A",
-              },
-              {
-                data: [4, -1, 1, 5, 8],
-                label: "B",
-              },
-            ]}
-            height={500}
-          />
+        <Grid2 size={{ xs: 12, md: 8 }}>
+          <Paper variant="outlined" square={false} sx={{ padding: 3 }}>
+            <DashTable></DashTable>
+          </Paper>
         </Grid2>
       </Grid2>
     </>
   );
 };
 
-const itemData = [
+const imageData = [
   {
     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
     title: "Breakfast",
+    data: ArrayNumbers(200, 8),
     rows: 2,
     cols: 2,
   },
