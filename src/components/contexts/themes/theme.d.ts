396176e8b41b761e516053e34254d94a3eed2de5
mysticblackmask <black.mask0209@outlook.com>
2024-11-11 03:41:53 -0800

    add types for theme

--- src/components/contexts/themes/theme.d.ts
+++ src/components/contexts/themes/theme.d.ts
@@ -2,3 +2,12 @@ export interface themeType {
   back: string;
   badge: string;
 }
+
+export interface clrType {
+  primary: themeType;
+  error: themeType;
+  info: themeType;
+  secondary: themeType;
+  success: themeType;
+  warning: themeType;
+}
